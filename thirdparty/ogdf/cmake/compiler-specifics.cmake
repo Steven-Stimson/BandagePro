ad5d572cbebb3e81eedce368c533993e74e80c8a
Anton Korobeynikov <anton@korobeynikov.info>
2025-12-08 11:27:23 -0800

    Build system gardening:  - Do not run MOC / UIC for non-Qt targets  - Do not use `-march=native` for ogdf

--- thirdparty/ogdf/cmake/compiler-specifics.cmake
+++ thirdparty/ogdf/cmake/compiler-specifics.cmake
@@ -13,10 +13,10 @@ if(MSVC)
 endif()
 
 # use native arch (ie, activate things like SSE)
-if(CMAKE_CXX_COMPILER_ID MATCHES "GNU" AND NOT ${CMAKE_SYSTEM_PROCESSOR} MATCHES "^arm")
-  # cannot use add_definitions() here because it does not work with check-sse3.cmake
-  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -march=native")
-endif()
+# if(CMAKE_CXX_COMPILER_ID MATCHES "GNU" AND NOT ${CMAKE_SYSTEM_PROCESSOR} MATCHES "^arm")
+#  # cannot use add_definitions() here because it does not work with check-sse3.cmake
+#  set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -march=native")
+# endif()
 
 # set default warning flags for OGDF and tests
 set(available_default_warning_flags "")
